59efbc05da4221b563e463327cc6dc0e1a9fc109
Enrique Segura <43327369+enriqueasegura@users.noreply.github.com>
2018-09-17 07:36:21 -0700

    Update move_my_beam.m

--- move_my_beam.m
+++ move_my_beam.m
@@ -1,4 +1,16 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%This function takes the final corrections from PI-controller 
+%to each pico-motor engine to move the beam to designed position
+%Currently it relies on a while loop to check whether the motor is moving or not
+%This is important because the limitations of the experiment
+%are that only one engine can be operated at a time by the driver.
+%For more information about each specific function, please
+%see manual on README
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
 function move_my_beam(rev_arr)
+
 lcaPutSmart('MOTR:B244:MC01:M0:CH1:MOTOR.TWV', rev_arr(1,1));
 lcaPutSmart('MOTR:B244:MC01:M0:CH1:MOTOR.TWF', 1.0);
 
